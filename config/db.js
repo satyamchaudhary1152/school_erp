d2ddc5c87ca869f3bb8a51e13c0614831c7010ed
Satyam Chaudhary <satyamchaudhary1152@gmail.com>
2026-07-23 23:39:40 +0530

    db configuration problem solved

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
+        console.log("URI:", process.env.MONGO_URL);
+
         await mongoose.connect(process.env.MONGO_URL);
+
         console.log("MongoDB Connected");
     } catch (error) {
-        console.log("Database Connection Failed");
-        console.error(error);
-        process.exit(1);
+        console.log(error);
     }
 };
 
